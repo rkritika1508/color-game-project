b26163922d73d9288adc6091c7277753a7531eeb
Kritika <rkritika1508@gmail.com>
2018-02-26 21:47:09 +0530

    Random colors out of predefined array can be selected, correct/wrong answer done

--- game.js
+++ game.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,8 +20,23 @@ for(var i=0; i<squares.length;i++){
 	squares[i].addEventListener("click", function(){
 		var clickedColor = this.style.backgroundColor;
 		if(clickedColor === pickedColor){
-			alert("Correct");
+			messageDisplay.textContent = "Correct";
+			changeColors(clickedColor);
 		}
-		else{alert("Wrong");}
+		else{
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try again";
+		}
 	})
+}
+
+function changeColors(color){
+	for(var i=0;i<squares.length;i++){
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
 }
