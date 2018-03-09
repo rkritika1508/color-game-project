cca3363bd085407191aadff27389ab12af974556
Kritika <rkritika1508@gmail.com>
2018-03-09 19:42:06 +0530

    Buttons Done. Styling almost completed

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -11,7 +12,8 @@ var hardBtn = document.querySelector("#hardBtn");
 easyBtn.addEventListener("click", function(){
 	hardBtn.classList.remove("selected");
 	easyBtn.classList.add("selected");
-	colors = generateRandomColors(3);
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
 	for(var i=0; i<squares.length; i++){
@@ -25,18 +27,19 @@ easyBtn.addEventListener("click", function(){
 hardBtn.addEventListener("click", function(){
 	hardBtn.classList.add("selected");
 	easyBtn.classList.remove("selected");	
-	colors = generateRandomColors(6);
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
 	for(var i=0; i<squares.length; i++){
 		squares[i].style.backgroundColor = colors[i];
-		squares[i].style.display = "none";
+		squares[i].style.display = "block";
 	}
 })
 
 resetButton.addEventListener("click", function(){
 	//generate new colors & pick random colors from array
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	//change color display to match picked color
 	colorDisplay.textContent = pickedColor;
@@ -44,7 +47,7 @@ resetButton.addEventListener("click", function(){
 	for(var i=0; i<squares.length;i++){
 		squares[i].style.backgroundColor = colors[i];
 	}
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
 })
 
 colorDisplay.textContent = pickedColor;
